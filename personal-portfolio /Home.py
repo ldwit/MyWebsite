9536967f6f97100c379131d61382cfb761dd6c4f
LT <laterral.williams@gmail.com>
2025-06-05 01:55:02 +0000

    .

--- personal-portfolio /Home.py
+++ personal-portfolio /Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.style import apply_global_styles
 from components.sidebar import render_sidebar
 
-st.set_page_config(page_title="Home", page_icon="🧿", layout="wide")
+st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
 # Apply styles globally
 apply_global_styles()
