23cab2c5a447a29aad42b0b15bf2169d202ffcc8
LT <laterral.williams@gmail.com>
2025-06-05 01:52:51 +0000

    v1.642025

--- personal-portfolio /Home.py
+++ personal-portfolio /Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.style import apply_global_styles
 from components.sidebar import render_sidebar
 
-st.set_page_config(page_title="Home", page_icon="📁", layout="wide")
+st.set_page_config(page_title="Home", page_icon="🧿", layout="wide")
 
 # Apply styles globally
 apply_global_styles()
@@ -10,4 +10,4 @@ apply_global_styles()
 render_sidebar()
 
 st.title("👋 Welcome to My Project Portfolio")
-st.write("Explore my work, skills, and more — all powered by Python + Streamlit.")
+st.write("Explore my work, skills, and more... All powered by Python + Streamlit.")
